e75fdbdf77e4b8373e9d1039afbf405e18813265
Winterflakes <sakshirs1234@gmail.com>
2023-08-07 11:10:17 +0530

    commit

---
+++ Exception_Handling/BankDemo.java
@@ -0,0 +1,60 @@
+package Exception_Handling;
+
+class InsufficientFundsException extends Exception {
+    private double amount;
+
+    public InsufficientFundsException(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
+
+class CheckingAccount {
+    private double balance;
+
+    public CheckingAccount(double amt) {
+        this.balance = amt;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            double needs = amount - balance;
+            throw new InsufficientFundsException(needs);
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getNumber() {
+        return 0;
+    }
+}
+
+public class BankDemo {
+    public static void main(String[] args) {
+        CheckingAccount c = new CheckingAccount(101);
+        System.out.println("Depositing $500...");
+        c.deposit(500.00);
+        try {
+            System.out.println("\nWithdrawing $100...");
+            c.withdraw(100.00);
+            System.out.println("\nWithdrawing $600...");
+            c.withdraw(600.00);
+        } catch (InsufficientFundsException e) {
+            System.out.println("Sorry, but you are short $"
+                    + e.getAmount());
+            e.printStackTrace();
+        }
+    }
+}
